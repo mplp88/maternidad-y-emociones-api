c12daf1e21cc43a6abae687ceadefcb19a0b1bb6
Martín Ponce <martinp88@gmail.com>
2025-08-30 04:22:36 -0300

    Agregado de likes y comentarios

--- routes/blogs.js
+++ routes/blogs.js
@@ -33,25 +33,103 @@ router.get("/:slug", async (req, res) => {
 });
 
 router.post("/", verifyToken, async (req, res) => {
-  const { title, content, author, summary, imageUrl, links } = req.body;
-  const blog = new Blog({ title, content, author, summary, imageUrl, links });
+  const { title, content, author, summary, imageUrls, links } = req.body;
+  const blog = new Blog({ title, content, author, summary, imageUrls, links });
   await blog.save();
   res.status(201).json(blog);
 });
 
-router.put('/:slug', verifyToken, async (req, res) => {
+router.put("/:slug", verifyToken, async (req, res) => {
   const { title, content, author, summary, imageUrls, slug, links } = req.body;
 
   try {
     const blog = await Blog.findOneAndUpdate(
       { slug },
-      { title, content, author, summary, imageUrls, links, updatedAt: Date.now() },
+      {
+        title,
+        content,
+        author,
+        summary,
+        imageUrls,
+        links,
+        updatedAt: Date.now(),
+      },
       { new: true }
     );
     if (!blog) return res.status(404).json({ error: "Post not found" });
     res.json({ blog });
   } catch (error) {
     console.error("Error updating blog:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.put("/:slug/comments", async (req, res) => {
+  const { slug } = req.params;
+  const { comment } = req.body;
+
+  if (!comment.author || !comment.content) {
+    return res
+      .status(400)
+      .json({ ok: false, error: "Author and content are required" });
+  }
+
+  try {
+    const blog = await Blog.findOne({ slug });
+    if (!blog) return res.status(404).json({ ok: false, error: "Post not found" });
+
+    blog.comments.push(comment);
+    await blog.save();
+
+    res.status(200).json({ ok: true, comment });
+  } catch (error) {
+    console.error("Error adding comment:", error);
+    res.status(500).json({ ok: false, error: "Internal server error" });
+  }
+});
+
+router.delete("/:slug/comments/:commentId", async (req, res) => {
+  const { slug, commentId } = req.params;
+
+  try {
+    const blog = await Blog.findOne({ slug });
+    if (!blog) return res.status(404).json({ ok: false, error: "Post not found" });
+
+    blog.comments = blog.comments.filter((comment) => comment._id.toString() !== commentId);
+    await blog.save();
+
+    res.status(200).json({ ok: true, comments: blog.comments });
+  } catch (error) {
+    console.error("Error deleting comment:", error);
+    res.status(500).json({ ok: false, error: "Internal server error" });
+  }
+});
+
+router.put("/:slug/like", async (req, res) => {
+  const { slug } = req.params;
+  const { clientId } = req.body;
+
+  if (!clientId) {
+    return res.status(400).json({ error: "Client ID is required" });
+  }
+
+  try {
+    const blog = await Blog.findOne({ slug });
+    if (!blog) return res.status(404).json({ error: "Post not found" });
+
+    const alreadyLiked = blog.likes.includes(clientId);
+
+    if (alreadyLiked) {
+      blog.likes = blog.likes.filter((id) => id !== clientId);
+      await blog.save();
+      return res.json({ liked: false });
+    } else {
+      blog.likes.push(clientId);
+      await blog.save();
+      return res.json({ liked: true });
+    }
+  } catch (error) {
+    console.error("Error adding like:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
